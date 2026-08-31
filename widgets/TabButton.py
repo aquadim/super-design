16f9890ffddc32292102e1e67dff37c4fa58078f
Вадим Королёв <aqua-accounts@mail.ru>
2026-08-31 22:47:30 +0300

    Внешний вид вкладок получше для других тем

--- widgets/TabButton.py
+++ widgets/TabButton.py
@@ -5,10 +5,12 @@ def get_notebook_tab_button():
     tab_box = Gtk.Box()
     tab_box.set_orientation(Gtk.Orientation.HORIZONTAL)
     tab_box.set_halign(Gtk.Align.START)
+    tab_box.set_spacing(4)
 
     label = Gtk.Label()
 
     close_btn = Gtk.Button.new_from_icon_name("window-close")
+    close_btn.add_css_class("flat")
 
     tab_box.append(label)
     tab_box.append(close_btn)
